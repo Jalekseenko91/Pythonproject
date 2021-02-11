8cf992ff4e22fe73750c9c0dfa2b73d8d9f4c446
Jalekseenko91 <jdalekseenko@gmail.com>
2021-02-11 11:31:11 +0100

    Testcases

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 
 from django.shortcuts import render
+from django.http import Http404
 from .models import Board
 
 def home(request):
@@ -11,5 +12,8 @@ def home(request):
     return render(request, 'home.html', {'boards': boards})
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
     return render(request, 'topics.html', {'board': board})
