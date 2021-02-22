538bff18221383e81733845ddba3d9a2ee5808e2
Jalekseenko91 <jdalekseenko@gmail.com>
2021-02-23 00:51:48 +0100

    refractoring forms page with using Django Forms API

--- myproject/boards/forms.py
+++ myproject/boards/forms.py
@@ -2,7 +2,11 @@ from django import forms
 from .models import Topic
 
 class NewTopicForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea(), max_length=4000)
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'Vertel hier iets leuks'}),
+        max_length=4000,
+        help_text='Maximale lengte mag niet meer zijn dan 4000 karacters.'
+    )
 
     class Meta:
         model = Topic
