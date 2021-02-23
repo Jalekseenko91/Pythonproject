c411cdf6d1af89748fc537eaf76b31adccf6396b
Jalekseenko91 <jdalekseenko@gmail.com>
2021-02-23 01:05:12 +0100

    refractoring new_topic page with Django widget tweaks

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Board, Topic, Post
 from .views import home, board_topics, new_topic
+from .forms import NewTopicForm
 
 
 class HomeTests(TestCase):
@@ -42,15 +43,7 @@ class BoardTopicsTests(TestCase):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
 
-    def test_board_topics_view_contains_navigation_links(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
-        homepage_url = reverse('home')
-        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
 
-        response = self.client.get(board_topics_url)
-
-        self.assertContains(response, 'href="{0}"'.format(homepage_url))
-        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
 
 class NewTopicTests(TestCase):
     def setUp(self):
@@ -115,3 +108,20 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+
+    def test_contains_form(self):  # <- new test
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
+    def test_new_topic_invalid_post_data(self):  # <- updated this one
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
