83111a14087ac8ec9b96253d5c04069d027c90d2
Jalekseenko91 <jdalekseenko@gmail.com>
2021-02-24 21:18:02 +0100

    improving the test layouts for accounts + adding background template for the signup page

--- myproject/accounts/tests/test_view_signup.py
+++ myproject/accounts/tests/test_view_signup.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
-from myproject.accounts.forms import SignUpForm
+
 from django.urls import reverse
 from django.urls import resolve
 from django.test import TestCase
-from myproject.accounts.views import signup
+from ..views import signup
+from ..forms import SignUpForm
 
 class SignUpTests(TestCase):
     def setUp(self):
