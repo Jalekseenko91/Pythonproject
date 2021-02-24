dc71d62ac461e61df4e8c2842ac8d7b0ce57fc70
Jalekseenko91 <jdalekseenko@gmail.com>
2021-02-25 00:33:09 +0100

    refractoring topic posts

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .forms import NewTopicForm
 from .models import Board, Topic, Post
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -20,22 +21,26 @@ def board_topics(request, pk):
         raise Http404
     return render(request, 'topics.html', {'board': board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user  # <- here
             topic.save()
-            post = Post.objects.create(
+            Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user  # <- and here
             )
             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    return render(request, 'topic_posts.html', {'topic': topic})
