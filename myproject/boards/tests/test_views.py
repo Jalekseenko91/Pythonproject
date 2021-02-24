d7f2aaeea9e9f09017c1721dba0114f060fec0d9
Jalekseenko91 <jdalekseenko@gmail.com>
2021-02-24 22:58:44 +0100

    refractoring tests + adding new templatetags test

--- myproject/boards/tests/test_views.py
+++ myproject/boards/tests/test_views.py
@@ -2,8 +2,9 @@ from django.test import TestCase
 from django.urls import resolve
 from django.urls import reverse
 from django.contrib.auth.models import User
-from ..models import Board, Topic, Post
+
 from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
 from ..forms import NewTopicForm
 
 
