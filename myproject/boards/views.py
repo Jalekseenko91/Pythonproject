dea5227e599d3c6a17e71a6f6b95ea8b4cff6bec
Jalekseenko91 <jdalekseenko@gmail.com>
2021-03-04 00:59:29 +0100

    added topic posts page

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,9 @@
 from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
-
+from django.shortcuts import redirect
+from django.views.generic import UpdateView
+from django.utils import timezone
 from .forms import NewTopicForm, PostForm
 from .models import Board, Post, Topic
 
@@ -60,3 +62,17 @@ def reply_topic(request, pk, topic_pk):
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
